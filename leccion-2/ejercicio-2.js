e160fa497f75b5db731c4c6413d5afb8b7d55564
moreescudero <escuderomore1@gmail.com>
2023-01-23 19:53:06 -0300

    ejercicio 2 finalizado

---
+++ leccion-2/ejercicio-2.js
@@ -0,0 +1,21 @@
+const libro = 
+{
+    titulo: "1984",
+    autor: "George Orwell",
+    fecha: new Date(1949, 6, 8),
+    url: new URL("https://es.wikipedia.org/wiki/1984_(novela)")
+}
+
+const lista = 
+[
+    nombre = "Morena",
+    edad = 21,
+    true,
+    new Date(2001, 6, 15),
+    libro.titulo,
+    libro.autor,
+    libro.fecha,
+    libro.url
+];
+
+console.log(lista);
